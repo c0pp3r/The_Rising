03b3421234ea1117ee1fdbfa40fc0ade855ff2ba
c0pp3r <dominguez.joey@gmail.com>
2016-09-13 23:37:39 -0400

    added in changes for BARC draft schem, still coming up as unknown schematic

--- MMOCoreORB/bin/scripts/managers/crafting/schematics.lua
+++ MMOCoreORB/bin/scripts/managers/crafting/schematics.lua
@@ -1423,6 +1423,7 @@ schematics = {
 {path="object/draft_schematic/structure/city/garden_exotic_endor.iff"},
 {path="object/draft_schematic/vehicle/civilian/landspeeder_x34.iff"},
 {path="object/draft_schematic/vehicle/civilian/speeder_bike_swoop.iff"},
+{path="object/draft_schematic/vehicle/civilian/shared_barc_speeder.iff"},
 {path="object/draft_schematic/vehicle/civilian/speeder_bike.iff"},
 {path="object/draft_schematic/vehicle/vehicle_customization_kit.iff"},
 {path="object/draft_schematic/space/chassis/z95.iff"},
--- MMOCoreORB/bin/scripts/object/draft_schematic/vehicle/civilian/objects.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/vehicle/civilian/objects.lua
@@ -579,3 +579,9 @@ object_draft_schematic_vehicle_civilian_shared_speeder_bike_swoop = SharedDraftS
 }
 
 ObjectTemplates:addClientTemplate(object_draft_schematic_vehicle_civilian_shared_speeder_bike_swoop, "object/draft_schematic/vehicle/civilian/shared_speeder_bike_swoop.iff")
+
+object_draft_schematic_vehicle_civilian_shared_barc_speeder = SharedDraftSchematicObjectTemplate:new {
+	clientTemplateFileName = "object/draft_schematic/vehicle/civilian/shared_barc_speeder.iff"
+}
+
+ObjectTemplates:addClientTemplate(object_draft_schematic_vehicle_civilian_shared_barc_speeder, "object/draft_schematic/vehicle/civilian/shared_barc_speeder.iff")
