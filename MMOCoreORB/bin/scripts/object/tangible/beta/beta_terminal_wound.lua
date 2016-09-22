aaabf1028e82a5396c612892daa2dbaa6107b893
c0pp3r <dominguez.joey@gmail.com>
2016-09-22 00:08:03 -0400

    Change reset buffs to Remove Buffs

--- MMOCoreORB/bin/scripts/object/tangible/beta/beta_terminal_wound.lua
+++ MMOCoreORB/bin/scripts/object/tangible/beta/beta_terminal_wound.lua
@@ -20,7 +20,7 @@ object_tangible_beta_beta_terminal_wound = object_tangible_beta_shared_beta_term
 	itemList = {
 			"Buff Character (10,000 Credits)", "enhance_character",
 			"Clense Character", "cleanse_character",
-			"Reset Buffs", "reset_buffs",
+			"Remove Buffs", "reset_buffs",
 			"Apply DOTs", "apply_dots",
 			"Clear DOTs", "clear_dots",
 			"Skills",
