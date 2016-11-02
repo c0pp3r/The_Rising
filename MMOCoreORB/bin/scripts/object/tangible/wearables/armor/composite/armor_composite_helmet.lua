6a37cc58593cfe1b8dffe1bda36515aeba847329
c0pp3r <dominguez.joey@gmail.com>
2016-11-02 08:24:12 -0400

    Restore the only half-removed (Toxic...) Electric special protection on composite armor

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/composite/armor_composite_helmet.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/composite/armor_composite_helmet.lua
@@ -89,6 +89,7 @@ object_tangible_wearables_armor_composite_armor_composite_helmet = object_tangib
 
 	-- Damage types in WeaponObject
 	vulnerability = LIGHTSABER,
+	specialResists = ELECTRICITY,
 
 	-- These are default Blue Frog stats
 	healthEncumbrance = 1,
@@ -114,8 +115,8 @@ object_tangible_wearables_armor_composite_armor_composite_helmet = object_tangib
 	experimentalWeights = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 	experimentalGroupTitles = {"null", "null", "null", "exp_durability", "exp_quality", "exp_resistance", "exp_durability", "exp_durability", "exp_durability", "null", "null", "exp_resistance", "null"},
 	experimentalSubGroupTitles = {"null", "null", "sockets", "hit_points", "armor_effectiveness", "armor_integrity", "armor_health_encumbrance", "armor_action_encumbrance", "armor_mind_encumbrance", "armor_rating", "armor_special_type", "armor_special_effectiveness", "armor_special_integrity"},
-	experimentalMin = {0, 0, 0, 1000, 1, 30000, 25, 22, 300, 1, 0, 0, 0},
-	experimentalMax = {0, 0, 0, 1000, 40, 50000, 15, 13, 180, 1, 0, 0, 0},
+	experimentalMin = {0, 0, 0, 1000, 1, 30000, 25, 22, 300, 1, 256, 0, 0},
+	experimentalMax = {0, 0, 0, 1000, 40, 50000, 15, 13, 180, 1, 256, 0, 0},
 	experimentalPrecision = {0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0},
 	experimentalCombineType = {0, 0, 4, 1, 1, 1, 1, 1, 1, 4, 4, 4, 1},
 }
