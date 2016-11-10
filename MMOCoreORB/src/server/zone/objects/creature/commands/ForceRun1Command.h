f24cd33ca24caad41faae0dc88c13ac1059eacfb
c0pp3r <dominguez.joey@gmail.com>
2016-11-10 00:06:02 -0500

    remove force run buff if skill box not found

--- MMOCoreORB/src/server/zone/objects/creature/commands/ForceRun1Command.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/ForceRun1Command.h
@@ -21,8 +21,12 @@ public:
     
     
 		// Skill mods.
-		skillMods.put("force_run", 1);
-		skillMods.put("slope_move", 33);
+		if(creature->hasSkill("force_discipline_enhancements_movement_02")){
+			skillMods.put("force_run", 1);
+			skillMods.put("slope_move", 33);
+		}else{
+			creature->sendSystemMessage("You lack sufficient skill to use the Force Run Command");
+		}
 	}
 
 	int doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) const {
@@ -33,11 +37,11 @@ public:
 			return GENERALERROR;
 		}
 		if (!creature->hasSkill("force_discipline_enhancements_movement_02")){
-			creature->sendSystemMessage("You lack sufficient skill to use the Force Run Command");
+			//creature->sendSystemMessage("You lack sufficient skill to use the Force Run Command");
 			creature->removeBuff(BuffCRC::JEDI_FORCE_RUN_1);
 			return GENERALERROR;
 		}
-		// Return if something is in error.
+		// Return if something is in error.heDqNA5F7XjY
 		if (res != SUCCESS) {
 			return res;
 		}
