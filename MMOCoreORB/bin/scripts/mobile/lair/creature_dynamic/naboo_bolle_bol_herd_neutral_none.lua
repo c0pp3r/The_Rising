28b66f8abc1a9e0ce560e80a370a72f15c219971
c0pp3r <dominguez.joey@gmail.com>
2016-11-30 09:04:55 -0500

    Fix lua error with bolle bols

--- MMOCoreORB/bin/scripts/mobile/lair/creature_dynamic/naboo_bolle_bol_herd_neutral_none.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/creature_dynamic/naboo_bolle_bol_herd_neutral_none.lua
@@ -1,7 +1,7 @@
 naboo_bolle_bol_herd_neutral_none = Lair:new {
 	mobiles = {{"bolle_bol",1},{"bolle_bol_female",1}},
 	spawnLimit = 15,
-	buildingsVeryEasy = {},bolle_bol_female
+	buildingsVeryEasy = {},
 	buildingsEasy = {},
 	buildingsMedium = {},
 	buildingsHard = {},
