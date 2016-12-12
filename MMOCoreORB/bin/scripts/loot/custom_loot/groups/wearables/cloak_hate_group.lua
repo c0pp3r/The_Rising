09669a0d778407ba70619e491e0e68c2ec587802
c0pp3r <dominguez.joey@gmail.com>
2016-12-11 22:27:40 -0500

    add new lootgroup for hate cloaks

---
+++ MMOCoreORB/bin/scripts/loot/custom_loot/groups/wearables/cloak_hate_group.lua
@@ -0,0 +1,11 @@
+cloak_hate_group = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = 0,
+	lootItems = {
+		{itemTemplate = "cloak_of_hate", weight = 5000000},
+		{itemTemplate = "cloak_of_hate_hood", weight = 5000000}
+	}
+}
+
+addLootGroupTemplate("cloak_hate_group", cloak_hate_group)
