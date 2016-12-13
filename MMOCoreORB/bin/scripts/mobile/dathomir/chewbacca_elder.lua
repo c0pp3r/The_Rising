a24d56f9d10fe09d52cce084e26f3e616e34783b
c0pp3r <dominguez.joey@gmail.com>
2016-12-12 23:47:27 -0500

    bump chewy and lower poggle

--- MMOCoreORB/bin/scripts/mobile/dathomir/chewbacca_elder.lua
+++ MMOCoreORB/bin/scripts/mobile/dathomir/chewbacca_elder.lua
@@ -2,8 +2,8 @@ chewbacca_elder = Creature:new {
 	customName = "Chewbacca (Elder)",
 	level = 300,
 	chanceHit = 85,
-	damageMin = 2000,
-	damageMax = 3310,
+	damageMin = 3000,
+	damageMax = 5310,
 	specialDamageMult = 25,
 	baseXp = 2784900,
 	baseHAM = 432100,
@@ -30,36 +30,80 @@ chewbacca_elder = Creature:new {
 			groups = {
 				{group = "g_carbine_heroic_sd", chance = 500000},
 				{group = "g_carbine_kun_massassi", chance = 500000},
-				{group = "g_polearm_heroic_sd", chance = 500000},
+				{group = "g_polearm_heroic_sd", chance = 750000},
 				{group = "g_pvp_bf_knuckler", chance = 500000},
-				{group = "g_sword_pvp_bf_01", chance = 500000},
+				{group = "g_sword_pvp_bf_01", chance = 750000},
 				{group = "g_pistol_heroic_exar", chance = 500000},
 				{group = "g_pistol_heroic_sd", chance = 500000},
 				{group = "g_som_2h_sword_obsidian", chance = 500000},
 				{group = "g_som_2h_sword_tulrus", chance = 500000},
 				{group = "g_som_sword_obsidian", chance = 500000},
 				{group = "weapons_all", chance = 4300000},
-				{group = "g_ep3_chiss_poacher_backpack", chance = 100000},
 				{group = "g_ep3_loot_necrosis", chance = 600000}
 			},
 			lootChance = 10000000
 		},
 		{
 			groups = {
-				{group = "av_21_pp", chance = 1000000}
+				{group = "cardeed1", chance = 1000000}
 			},
-			lootChance = 500000
+			lootChance = 1000000
 		 },
 		  {
 			groups = {
-				{group = "crystals_premium", chance = 2500000},
 				{group = "clothing_attachments", chance = 2000000},
 				{group = "armor_attachments", chance = 2000000},
-				{group = "g_named_crystals", chance = 1000000},
+				{group = "droid_loot", chance = 1000000},
 				{group = "weapons_all", chance = 2500000}
 		},
 			lootChance = 10000000
 		  },
+		  {
+			groups = {
+				{group = "clothing_attachments", chance = 2000000},
+				{group = "armor_attachments", chance = 2000000},
+				{group = "droid_loot", chance = 1000000},
+				{group = "weapons_all", chance = 2500000}
+		},
+			lootChance = 10000000
+		  },
+		  {
+			groups = {
+				{group = "clothing_attachments", chance = 2000000},
+				{group = "armor_attachments", chance = 2000000},
+				{group = "droid_loot", chance = 1000000},
+				{group = "weapons_all", chance = 2500000}
+		},
+			lootChance = 10000000
+		  },
+		  {
+			groups = {
+				{group = "armor_all", chance = 2500000},
+				{group = "clothing_attachments", chance = 2000000},
+				{group = "armor_attachments", chance = 2000000},
+				{group = "weapons_all", chance = 2500000}
+		},
+			lootChance = 10000000
+		  },
+		  {
+			groups = {
+				{group = "armor_all", chance = 2500000},
+				{group = "clothing_attachments", chance = 2000000},
+				{group = "armor_attachments", chance = 2000000},
+				{group = "weapons_all", chance = 2500000}
+		},
+			lootChance = 10000000
+		  },
+		  {
+			groups = {
+				{group = "armor_all", chance = 2500000},
+				{group = "clothing_attachments", chance = 2000000},
+				{group = "armor_attachments", chance = 2000000},
+				{group = "weapons_all", chance = 2500000}
+		},
+			lootChance = 10000000
+		  },
+		  
 	},
 	weapons = {"chewbacca_weapons"},
 	conversationTemplate = "",
