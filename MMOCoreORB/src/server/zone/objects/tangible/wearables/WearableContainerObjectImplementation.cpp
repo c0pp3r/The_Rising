c5229c1a29402f6bd1c69312dbe6b5c1b5c552f4
c0pp3r <dominguez.joey@gmail.com>
2016-12-21 14:46:38 -0500

    add debugging

--- MMOCoreORB/src/server/zone/objects/tangible/wearables/WearableContainerObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/wearables/WearableContainerObjectImplementation.cpp
@@ -41,9 +41,11 @@ void WearableContainerObjectImplementation::applySkillModsTo(CreatureObject* cre
 
 		if (!SkillModManager::instance()->isWearableModDisabled(name)){
 			if (name == "jedi_toughness" || name == "jedi_state_defense" || name == "force_defence"){
+				info("Is a mod restricted to Jedi: "+name , true);
 				ManagedReference<PlayerObject*> creatureGhost = creature->getPlayerObject();
 				if (creatureGhost != NULL){
 					if (creatureGhost->getJediState() <= 1)
+						info("Player is not a Jedi");
 						return;
 				}
 			}
