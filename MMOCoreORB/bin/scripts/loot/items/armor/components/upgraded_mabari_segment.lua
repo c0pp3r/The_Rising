24a3579b99b6cb789dba59494b94b5fc4abd6a85
c0pp3r <dominguez.joey@gmail.com>
2017-01-24 14:31:10 -0500

    forgot to add segment

---
+++ MMOCoreORB/bin/scripts/loot/items/armor/components/upgraded_mabari_segment.lua
@@ -0,0 +1,17 @@
+customObjectName = "Upgraded Mabari Segment",
+	directObjectTemplate = "object/tangible/component/armor/armor_segment_zam_advanced.iff",
+	craftingValues = {
+		{"armor_special_type",0,0,0},
+		{"armor_effectiveness",10,33,10},
+		{"kineticeffectiveness",35,43,10},
+		{"armor_integrity",1400,1577,0},
+		{"armor_action_encumbrance",52,45,0},
+		{"armor_mind_encumbrance",52,45,0},
+		{"armor_health_encumbrance",52,45,0},
+		{"useCount",1,10,0},
+	},
+	customizationStringNames = {},
+	customizationValues = {}
+}
+
+addLootItemTemplate("upgraded_mabari_segment", upgraded_mabari_segment)
