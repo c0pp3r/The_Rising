93b4210598085613eca4bbe3bb2b52b815e42584
c0pp3r <dominguez.joey@gmail.com>
2017-01-29 23:38:35 -0500

    fix missing curly brace

--- MMOCoreORB/src/server/zone/objects/scene/components/ContainerComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/scene/components/ContainerComponent.cpp
@@ -37,18 +37,19 @@ int ContainerComponent::canAddObject(SceneObject* sceneObject, SceneObject* obje
 				if (buio != NULL ) {
 					uint64 bid = buio->getOwnerObjectID();
 
-				if ((containerPlayerParent != NULL && bid != containerPlayerParent->getObjectID()) || (sceneObject->isPlayerCreature() && bid != sceneObject->getObjectID())) {
-					errorDescription = "@container_error_message:container27";
-					return TransferErrorCode::CANTREMOVE;
+					if ((containerPlayerParent != NULL && bid != containerPlayerParent->getObjectID()) || (sceneObject->isPlayerCreature() && bid != sceneObject->getObjectID())) {
+						errorDescription = "@container_error_message:container27";
+						return TransferErrorCode::CANTREMOVE;
+					}
 				}
-			}
 			} else if (objPlayerParent != NULL && containerPlayerParent == NULL && containerBuildingParent != NULL && !sceneObject->isPlayerCreature()) {
 			ManagedReference<BuildingObject*> buio = cast<BuildingObject*>( containerBuildingParent.get());
 
-			if (buio != NULL && buio->getOwnerObjectID() != objPlayerParent->getObjectID()) {
+				if (buio != NULL && buio->getOwnerObjectID() != objPlayerParent->getObjectID()) {
 
-				errorDescription = "@container_error_message:container28";
-				return TransferErrorCode::CANTADD;
+					errorDescription = "@container_error_message:container28";
+					return TransferErrorCode::CANTADD;
+				}
 			}
 		}
 	}
