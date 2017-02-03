3e3be2cf9f4942ca4e601dc8424ac3cc4f875630
c0pp3r2 <dominguez.joey@gmail.com>
2017-02-02 22:36:13 -0500

    changed flurry to 4.5 multiplier and Phantom/dervish2 to AoE

--- MMOCoreORB/bin/scripts/commands/saberPolearmDervish2.lua
+++ MMOCoreORB/bin/scripts/commands/saberPolearmDervish2.lua
@@ -49,9 +49,8 @@ SaberPolearmDervish2Command = {
 	healthCostMultiplier = 0,
 	actionCostMultiplier = 0,
 	mindCostMultiplier = 0,
-	coneAngle = 45,
-	coneAction = true,
-	coneRange = 24,
+	areaRange = 16,
+	areaAction = true,
 	forceCostMultiplier = 3.0,
 	visMod = 25,
 
