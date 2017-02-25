919c115d6580e0e27f7b18a05e053d791c7c9190
c0pp3r2 <dominguez.joey@gmail.com>
2017-02-24 23:35:42 -0500

    fix chewy social group

--- MMOCoreORB/bin/scripts/mobile/dathomir/chewbacca_elder.lua
+++ MMOCoreORB/bin/scripts/mobile/dathomir/chewbacca_elder.lua
@@ -1,5 +1,7 @@
 chewbacca_elder = Creature:new {
 	customName = "Chewbacca (Elder)",
+	socialGroup = "gondula_tribe",
+	faction = "gondula_tribe",
 	level = 300,
 	chanceHit = 85,
 	damageMin = 3300,
